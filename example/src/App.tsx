8474e5201bbb86be54041a8c6cf09ea0885a953e
malsuke <malsuke096@gmail.com>
2026-06-16 21:00:57 +0900

    first commit

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+import HexViewer from 'react-hex-viewer'
+function App() {
+
+  return (
+    <>
+    <HexViewer hexString="48656c6c6f20576f726c6421" />
+    </>
+  )
+}
+
+export default App
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { HexViewer } from './hex-viewer'
+import type { HexViewerProps } from './hex-viewer'
+
+export { HexViewer }
+export type { HexViewerProps }
