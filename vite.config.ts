20be7e1b89b1b2fc82d384f5e865f444782e3f0a
malsuke <malsuke096@gmail.com>
2026-06-17 08:57:24 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,58 @@
+/// <reference types="vitest/config" />
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    cssInjectedByJsPlugin(),
-    dts({
-      tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
-      entryRoot: "src",
-    }),
-  ],
+  plugins: [cssInjectedByJsPlugin(), dts({
+    tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
+    entryRoot: "src"
+  })],
   build: {
     cssCodeSplit: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "ReactHexViewer",
       formats: ["es", "umd"],
-      fileName: "react-hex-viewer",
+      fileName: "react-hex-viewer"
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
       output: {
         globals: {
           react: "React",
-          "react/jsx-runtime": "jsxRuntime",
-        },
-      },
-    },
+          "react/jsx-runtime": "jsxRuntime"
+        }
+      }
+    }
   },
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
+  }
 });
